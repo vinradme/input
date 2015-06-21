106206ba697eed9688d2a0e19c258cf5ae930a39
Vinodhini Ravikumar <vinravik@cisco.com>
2015-06-21 14:37:51 -0700

    added input.py

---
+++ input.py
@@ -0,0 +1,6 @@
+age = int(raw_input("What is your age?>"))
+name = raw_input("What is your name?>")
+age_in_10 = age + 10
+print "Hi {}, you are {} years of age".format(name, age)
+print "In 10 years, you will be, {} years old".format(age_in_10)
+
